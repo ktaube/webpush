40b1eb397057f72b9bfc271cf8c781c47f6e7189
Edy Musajevs <edymusajevs@Edys-MacBook-Pro.local>
2025-07-09 13:58:42 +0300

    minimal css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,33 +8,39 @@ function App({ user, onLogout }: { user: User; onLogout: () => void }) {
   const { isSubscribed, subscribe, unsubscribe } = useNotifications();
 
   return (
-    <>
-      <h1>Push Notifications</h1>
-      <p>Logged in as {user.username}</p>
-      <p>Active subscriptions: {user.subscriptions.length}</p>
-      <ul>
-        {user.subscriptions.map((subscription) => (
-          <li key={subscription.endpoint}>{subscription.endpoint}</li>
-        ))}
-      </ul>
-      <button onClick={onLogout}>Logout</button>
-      {isSubscribed ? (
-        <button onClick={unsubscribe}>unsubscribe</button>
-      ) : (
-        <button
-          onClick={async () => {
-            const permission = await Notification.requestPermission();
-            console.log(permission);
-            if (permission !== "granted") return;
-            await subscribe();
-          }}
-        >
-          subscribe
-        </button>
-      )}
+    <main>
+      <header>
+        <h1>Push Notifications</h1>
+        <p>Logged in as {user.username}</p>
+      </header>
+      <section>
+        <p>Active subscriptions: {user.subscriptions.length}</p>
+        <ul>
+          {user.subscriptions.map((subscription) => (
+            <li key={subscription.endpoint}>{subscription.endpoint}</li>
+          ))}
+        </ul>
+      </section>
+      <nav>
+        <button onClick={onLogout}>Logout</button>
+        {isSubscribed ? (
+          <button onClick={unsubscribe}>unsubscribe</button>
+        ) : (
+          <button
+            onClick={async () => {
+              const permission = await Notification.requestPermission();
+              console.log(permission);
+              if (permission !== "granted") return;
+              await subscribe();
+            }}
+          >
+            subscribe
+          </button>
+        )}
+      </nav>
       <SubscriberList />
       <PWABadge />
-    </>
+    </main>
   );
 }
 
