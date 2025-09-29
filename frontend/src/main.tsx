a870538dc72d3df327bb945bb6b1709c803b7167
Kristaps Taube <kristaps.taube@crownroadsoftware.com>
2025-09-29 18:15:31 +0300

    chore: add react-query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,6 +15,9 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -32,7 +35,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   );
 }
